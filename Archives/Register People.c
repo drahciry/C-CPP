07c8e3b546db758afbcd662184095be4de6e3972
drahciry <richardgonric@gmail.com>
2025-06-16 01:11:45 -0300

    Update folder 'Archives': register people. Implementing of binary archive.

---
+++ Archives/Register People.c
@@ -0,0 +1,143 @@
+// Includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Tipo 'Person' do tipo struct
+typedef struct {
+    char name[50];
+    int age;
+} Person;
+
+// Função que realiza leitura dinâmica do teclado
+char* readline() {
+    // Declaração de variáveis
+    int ch, len = 0, cap = 10;
+    char* temp;
+    char* line = malloc(cap);
+    // Retorna erro se não alocar
+    if (!line) {
+        fprintf(stderr, "Erro: falha ao alocar memória!\n\n");
+        exit(1);
+    }
+
+    // Leitura de caracter por caracter
+    while ((ch = getchar()) != '\n' && ch != EOF) {
+        // Realoca caso esteja cheio
+        if (len >= cap - 2) {
+            cap *= 2;
+            temp = realloc(line, cap);
+            while (!temp) temp = realloc(line, cap);
+            line = temp;
+        }
+        line[len++] = ch;
+    }
+    // Adiciona terminador no fim da string
+    line[len] = '\0';
+    // Retorna a leitura feita
+    return line;
+}
+
+// Função que abre arquivo binário e salva Person
+void writeFile(char* path, Person* p, int cap) {
+    // Declaração de variáveis
+    FILE* file = fopen(path, "wb");
+    // Retorna erro caso não seja possível abrir arquivo
+    if (!file) {
+        fprintf(stderr, "Erro: falha ao abrir arquivo!\n\n");
+        exit(3);
+    }
+
+    // Salva a quantidade de pessoas que serão salvas no arquivo
+    fwrite(&cap, sizeof(int), 1, file);
+    // Salva pessoas do tipo Person no arquivo aberto
+    for (int i = 0; i < cap; i++) fwrite(p + i, sizeof(Person), 1, file);
+    // Fecha arquivo
+    fclose(file);
+}
+
+// Função que inicializa array de Person
+Person* createPeople(int cap) {
+    // Inicializa array de Person
+    Person* p = malloc(cap * sizeof(Person));
+    if (!p) {
+        fprintf(stderr, "Erro: falha ao alocar memória!\n\n");
+        exit(2);
+    }
+    // Retorna array de Person
+    return p;
+}
+
+// Função que cadastra pessoa (Person)
+void registerPerson(Person* p) {
+    printf("Insira o nome da pessoa a ser registrada: ");
+    fgets(p->name, sizeof(p->name), stdin);
+    p->name[strcspn(p->name, "\n")] = '\0';
+    printf("Insira a idade da pessoa: ");
+    scanf("%d", &p->age);
+    printf(
+        "\nPessoa registrada com sucesso:\n"
+        "Nome: %s | Idade: %d\n\n",
+        p->name, p->age
+    );
+}
+
+// Realoca array de Person
+void reallocPerson(Person** p, int* cap) {
+    *cap *= 2;
+    Person* temp = realloc(*p, *cap * sizeof(Person));
+    while (!temp) temp = realloc(*p, *cap * sizeof(Person));
+    *p = temp;
+}
+
+// Função principal (main)
+int main() {
+    // Declaração de variáveis
+    int option = -1, len = 0, cap = 2;
+    char* path;
+    char* temp;
+    Person* p = createPeople(cap);
+
+    printf("\n---------- Registro de Pessoas - Arquivo Binário ----------\n\n");
+    // Menu "Interativo"
+    while (option != 0) {
+        printf(
+            "Opções disponíveis:\n"
+            "[1] Registrar pessoa\n"
+            "[0] Encerrar programa\n\n"
+            "Opção desejada: "
+        );
+        scanf("%d", &option);
+        while(getchar() != '\n');
+        printf("\n");
+        switch (option) {
+            // Registra pessoa no sistema
+            case 1:
+                // Realoca memória caso esteja cheio
+                if (len >= cap - 1) {
+                    cap *= 2;
+                    reallocPerson(&p, &cap);
+                }
+                registerPerson(p + len);
+                len++;
+                break;
+            // Encerra programa
+            case 0:
+                break;
+            // Opções inválidas
+            default:
+                printf("\nOpção inserida inválida. Insira uma opção válida.\n");
+        }
+    }
+    // Captura nome do arquivo que deseja salvar dados
+    printf("Insira o nome do arquivo que deseja salvar pessoas registradas: ");
+    path = readline();
+    // Salva array de Person em arquivo binário
+    writeFile(path, p, len);
+    // Feedback
+    printf("\nPessoas salvas com sucesso!\n\n");
+    // Libera memória
+    free(p);
+    free(path);
+    return 0;
+}
