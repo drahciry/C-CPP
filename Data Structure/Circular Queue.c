4dfb569af51a020e953433496105c0a86bf31418
drahciry <richardgonric@gmail.com>
2025-06-18 02:16:58 -0300

    Update folder 'Data Structures': circular queue and stack. I started data structures.

---
+++ Data Structure/Circular Queue.c
@@ -0,0 +1,124 @@
+// Includes
+#include <stdio.h>
+#include <stdlib.h>
+
+// Tipo Fila
+typedef struct {
+    int size, cap, front, rear;
+    int* queue;
+} Queue;
+
+// Erro de alocação de memória
+void MallocError() {
+    fprintf(stderr, "Erro: falha ao alocar memória!\n");
+    exit(1);
+}
+
+// Erro de fila vazia
+void EmptyQueueError() {
+    fprintf(stderr, "Erro: fila está vazia!\n");
+    exit(2);
+}
+
+// Erro de fila cheia
+void FullQueueError() {
+    fprintf(stderr, "Erro: fila está cheia!\n");
+    exit(3);
+}
+
+// Função que inicializa fila
+Queue* createQueue(int cap) {
+    // Declaração de variáveis
+    Queue* q = malloc(sizeof(Queue));
+    // Retorna erro caso não seja alocado
+    if (!q) MallocError();
+    
+    // Inicializa array com capacidade informada
+    q->queue = malloc(cap * sizeof(int));
+    // Retorna erro caso não seja alocado
+    if (!q->queue) MallocError();
+    q->cap = cap;
+    q->size = 0;
+    q->front = -1;
+    q->rear = -1;
+    // Retorna a fila
+    return q;
+}
+
+// Função que verifica se fila está vazia
+int isEmpty(Queue* q) {
+    if (!q->size) return 1;
+    return 0;
+}
+
+// Função que verifica se fila está cheia
+int isFull(Queue* q) {
+    if (q->size == q->cap) return 1;
+    return 0;
+}
+
+// Função que enfila dados
+void enqueue(Queue* q, int value) {
+    // Verifica se fila está cheia
+    if (isFull(q)) FullQueueError();
+    // Verifica se fila está vazia
+    if (isEmpty(q)) {
+        q->front = 0;
+        q->rear = 0;
+        q->queue[q->rear] = value;
+        q->size++;
+        return;
+    }
+    // Adiciona valor à fila
+    q->rear = (q->rear + 1) % q->cap;
+    q->queue[q->rear] = value;
+    q->size++;
+}
+
+// Função que desenfila dados
+int dequeue(Queue* q) {
+    // Declaração de variáveis
+    int value;
+
+    // Verifica se fila está vazia
+    if (isEmpty(q)) EmptyQueueError();
+    // Remove valor da fila
+    value = q->queue[q->front];
+    q->front = (q->front + 1) % q->cap;
+    q->size--;
+    // Verifica se fila ficou vazia
+    if (isEmpty(q)) {
+        q->front = -1;
+        q->rear = -1;
+    }
+    // Retorna valor desenfilado
+    return value;
+}
+
+// Função para retornar tamanho da pilha
+int lenQueue(Queue* q) {
+    return q->size;
+}
+
+// Função que exibe fila
+void displayQueue(Queue* q) {
+    // Verifica se fila está vazia
+    if (isEmpty(q)) EmptyQueueError();
+    // Percorre array para exibir dados
+    for (int i = q->front;; i = (i + 1) % q->cap) {
+        printf("%d; ", q->queue[i]);
+        if (i == q->rear) break;
+    }
+    printf("\n");
+}
+
+// Função para deletar fila
+void deleteQueue(Queue* q) {
+    free(q->queue);
+    free(q);
+}
+
+// Função principal (main)
+int main() {
+    return 0;
+}
---
+++ Data Structure/Stack.c
@@ -0,0 +1,116 @@
+// Includes
+#include <stdio.h>
+#include <stdlib.h>
+
+// Tipo Pilha
+typedef struct {
+    int cap, top;
+    int* stack;
+} Stack;
+
+// Erro de alocação de memória
+void MallocError() {
+    fprintf(stderr, "Erro: falha ao alocar memória!\n");
+    exit(1);
+}
+
+// Erro de pilha vazia
+void EmptyStackError() {
+    fprintf(stderr, "Erro: pilha está vazia!\n");
+    exit(2);
+}
+
+// Erro de pilha cheia
+void FullStackError() {
+    fprintf(stderr, "Erro: pilha está cheia!\n");
+    exit(3);
+}
+
+// Função que inicializa pilha
+Stack* createStack(int cap) {
+    // Declaração de variáveis
+    Stack* s = malloc(sizeof(Stack));
+    // Retorna erro caso não seja alocado
+    if (!s) MallocError();
+    
+    // Inicializa array com capacidade informada
+    s->stack = malloc(cap * sizeof(int));
+    // Retorna erro caso não seja alocado
+    if (!s->stack) MallocError();
+    s->cap = cap;
+    s->top = -1;
+    // Retorna a pilha
+    return s;
+}
+
+// Função que verifica se pilha está vazia
+int isEmpty(Stack* s) {
+    if (s->top == -1) return 1;
+    return 0;
+}
+
+// Função que verifica se pilha está cheia
+int isFull(Stack* s) {
+    if (s->top + 1 == s->cap) return 1;
+    return 0;
+}
+
+// Função que empilha valor
+void push(Stack* s, int value) {
+    // Verifica se pilha está cheia
+    if (isFull(s)) FullStackError();
+    // Verifica se pilha está vazia
+    if (isEmpty(s)) {
+        s->top = 0;
+        s->stack[s->top] = value;
+        return;
+    }
+    // Adiciona valor à pilha
+    s->stack[++s->top] = value;
+}
+
+// Função para desempilhar valor
+int pop(Stack* s) {
+    // Declaração de variáveis
+    int value;
+
+    // Verifica se pilha está vazia
+    if (isEmpty(s)) EmptyStackError();
+    // Remove valor da pilha
+    value = s->stack[s->top];
+    s->top--;
+    // Retorna valor desempilhado
+    return value;
+}
+
+// Função para retornar topo
+int top(Stack* s) {
+    return s->stack[s->top];
+}
+
+// Função para retornar tamanho da pilha
+int lenStack(Stack* s) {
+    return s->top + 1;
+}
+
+// Função para exibir a pilha
+void displayStack(Stack* s) {
+    // Verifica se pilha está vazia
+    if (isEmpty(s)) EmptyStackError();
+    // Percorre array para exibir dados
+    for (int i = s->top; i >= 0; i--) {
+        printf("%d; ", s->stack[i]);
+    }
+    printf("\n");
+}
+
+// Função para deletar pilha
+void deleteStack(Stack* s) {
+    free(s->stack);
+    free(s);
+}
+
+// Função principal (main)
+int main() {
+    return 0;
+}
